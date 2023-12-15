0a7c7dc3523e680d668f2c6c0bfd0ba4b3184755
Prakrit Uprety <prakritupreety0@gmail.com>
2023-12-15 12:33:59 -0800

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import { useSelector,useDispatch } from 'react-redux'
+import {signInStart,signInSuccess,signInFailure} from './../redux/user/user.slice';
 
 export default function SignIn() {
-
+  const dispatch = useDispatch();
   const [formData,setFormData] = useState({})
-  const [loading, setLoading] = useState(false)
-  const [error,setError] = useState(null)
+  const {loading,error} = useSelector((state)=>state.user)
   const navigate = useNavigate();
   
   const handleChange = (e)=>{
@@ -16,7 +17,7 @@ export default function SignIn() {
 
   const handleSignIn =async(e)=>{
     e.preventDefault();
-    setLoading(true)
+    dispatch(signInStart())
     const{email,password} = formData;
     const loginData = {email,password}
     console.log(loginData)
@@ -32,16 +33,14 @@ export default function SignIn() {
       })
       const data = await res.json()
       if(data.success == false){
-        setLoading(false);
-        setError(data.message)
+        dispatch(signInFailure(data.message))
         return 
       }
-      setLoading(false)
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate('/about')
 
     }catch(error){
-      setError(error.message);
+      dispatch(signInFailure(error.message))
     }
 
   }
@@ -49,9 +48,9 @@ export default function SignIn() {
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='text-center text-slate-700 text-4xl font-bold my-3'>Sign In</h1>
-      <form className="flex flex-col gap-2">
-        <input onChange={handleChange} id="email" type="text" placeholder='john@example.com' className='p-3' />
-        <input onChange = {handleChange} id="password" type="password" placeholder='********' className='p-3' />
+      <form className="flex flex-col gap-4">
+        <input onChange={handleChange} id="email" type="text" placeholder='john@example.com' className='p-3 rounded-lg' />
+        <input onChange = {handleChange} id="password" type="password" placeholder='********' className='p-3 rounded-lg' />
         <button disabled={loading} onClick={handleSignIn} className='bg-slate-700 text-slate-100 rounded-lg p-3 hover:opacity-95 disabled:opacity-80' >Sign In</button>
       </form>
       <div className='flex gap-2 my-2'>
