639089e5f5933d9cffb6732e3155195aa78b3702
Prakrit Uprety <prakritupreety0@gmail.com>
2023-12-14 17:17:37 -0800

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import SignUp from './pages/SIgnUp'
 
+import Header from './components /Header'
 
 function App() {
   return <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path = "/" element={<Home/>}/>
       <Route path ="/sign-in" element={<SignIn/>}/>
