ef73c9838584bc45d6dff72b87ce383dcf60ea79
Prakrit Uprety <prakritupreety0@gmail.com>
2023-12-15 09:36:24 -0800

    complete signup page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -7,7 +7,7 @@ export const signUp = async (req,res,next)=>{
 
     const {username, email, password} = req.body;
     const hash = bcryptjs.hashSync(password,10)
-    const newUser = new User({username,email,password: hash})
+    const newUser = new User({username,email,password:hash})
     try{
 
         await newUser.save();
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
+
+  const [formData,setFormData] = useState({})
+  const [loading,setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const navigate = useNavigate();
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+  }
+
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+    setLoading(true)
+    try{
+      const res = await fetch('/api/auth/signup',{
+        method: 'POST',
+        headers : {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success == false){
+        setLoading(false)
+        setError(data.message)
+        return
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+    }
+    catch(err){
+      setLoading(false)
+      setError(err.message);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-bold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4 '>
-        <input id="username" type="text" placeholder='username' className='border p-3 rounded-lg'/>
-        <input id="email" type="email" placeholder='abc@example.com' className='border p-3 rounded-lg'/>
-        <input id="password" type="password" placeholder='*******' className='border p-3 rounded-lg'/>
-        <button  className='disabled bg-slate-700 p-3 rounded-lg text-slate-100 uppercase hover:opacity-95 disabled:opacity-80 '>Sign Up</button>
+        <input onChange={handleChange} id="username" type="text" placeholder='username' className='border p-3 rounded-lg'/>
+        <input onChange={handleChange} id="email" type="email" placeholder='abc@example.com' className='border p-3 rounded-lg'/>
+        <input onChange={handleChange} id="password" type="password" placeholder='*******' className='border p-3 rounded-lg'/>
+        <button disabled={loading} onClick={handleSubmit} className='disabled bg-slate-700 p-3 rounded-lg text-slate-100 uppercase hover:opacity-95 disabled:opacity-80 '>Sign Up</button>
       </form>
       <div className='flex gap-5 mt-5'>
         <p>Have an account?</p>
@@ -17,6 +56,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign In</span>
         </Link>
       </div>
+      {error && <p className='text-red-700'>{error}</p>}
     </div>
   )
 }
