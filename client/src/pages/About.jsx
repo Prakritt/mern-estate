20636cc23fbf7b45b2b33bca604e599edc8b1cd6
Prakrit Uprety <prakritupreety0@gmail.com>
2023-12-27 12:09:37 -0800

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className=''>
+      <div className='max-w-6xl mx-auto mt-4'>
+        <h1 className='mb-4 text-3xl font-bold text-slate-800'>About Real Estate</h1>
+        <p className='flex flex-col gap-4 text-slate-700 text-justify'>
+
+          At Real Estate, we are more than just a property firm; we are your gateway to exceptional living and investing. With a commitment to excellence and a passion for helping individuals and families find their dream homes or lucrative investment opportunities, we stand as a beacon of trust and reliability in the real estate industry. Our journey is rooted in a dedication to customer satisfaction, transparency, and innovation, ensuring that each client experiences a seamless and rewarding real estate journey.
+          <div className="">
+
+            <h2 className='text-1xl font-bold text-slate-800'>Our Vision:</h2>
+
+            Real Estate envisions a world where every individual can find a space that resonates with their lifestyle, aspirations, and investment goals. Whether you are seeking a cozy family home, a vibrant urban apartment, or a strategic investment property, we strive to provide a diverse portfolio to cater to your unique needs. Our vision extends beyond the transactional aspect of real estate, aiming to create communities and build relationships that last a lifetime.
+          </div>
+
+          <div className="">
+            <h2 className='text-1xl font-bold text-slate-800'>Why Choose Real Estate?</h2>
+
+            At Real Estate, our success is built on a foundation of integrity, expertise, and a genuine passion for real estate. Our team of experienced professionals is dedicated to guiding you through every step of your real estate journey, offering personalized advice and solutions. We leverage cutting-edge technology and market insights to ensure that our clients make informed decisions, whether buying, selling, or investing. Join us at Real Estate, where your property dreams become reality.
+
+          </div>
+        </p>
+        </div>
+    </div>
   )
 }
